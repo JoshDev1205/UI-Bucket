ba10f9ecbc5b5366ca3aae9d89b71cd7835fabdb
Gustavo García <guzgki@gmail.com>
2018-07-26 14:09:32 -0500

    add index file to load all stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,16 +1,7 @@
-import React from 'react';
-import '../index';
-import { storiesOf } from '@storybook/react';
+import { configure } from '@storybook/react'
 
-storiesOf('Button', module)
-  .add('Basic buttons', () => (
-    <div className="buttons-container">
-      <mate-button type="primary">Primary button</mate-button>
-      <mate-button
-        type="secondary"
-        outline
-      >
-        Secondary button
-      </mate-button>
-    </div>
-  ))
+const req = require.context(__dirname, true, /\.story\.js$/)
+
+configure(() => {
+  req.keys().forEach((filename) => req(filename))
+}, module)
